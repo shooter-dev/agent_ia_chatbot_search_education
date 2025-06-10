2d171d0aaff9e1feb80dc3695422bf4c06e7d27e
shooter-dev <vincentbleach@gmail.com>
2025-06-10 10:14:54 +0200

    feat : create LlmRag && ILmRag

---
+++ src/services/llm_rag/llm_rag.py
@@ -0,0 +1,5 @@
+from src.services.llm_rag.llm_rag_interface import ILlmRag
+
+
+class LlmRag(ILlmRag):
+    pass
---
+++ src/services/llm_rag/llm_rag_interface.py
@@ -0,0 +1,6 @@
+from abc import abstractmethod, ABC
+
+
+class ILlmRag(ABC):
+
+    pass

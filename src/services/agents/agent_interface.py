4ae076adcd899c6c1f9e1152ccc974fd25b04ab3
Elvis <elvism72@gmail.com>
2025-06-14 10:07:07 +0200

    mise à jour interface

--- src/services/agents/agent_interface.py
+++ src/services/agents/agent_interface.py
@@ -1,6 +1,25 @@
-from abc import ABC
+import abc
+from typing import List
+from langchain_core.tools import Tool
 
+class IAgent(abc.ABC):
 
-class IAgent(ABC):
-    def __init__(self):
+    @abc.abstractmethod
+    def create_react_agent(self, tools: List[Tool]):
+        pass
+
+    @abc.abstractmethod
+    def executor(self, tools: List[Tool]):
+        pass
+
+    @abc.abstractmethod
+    def poser_question(self, question: str, question_type: str) -> str:
+        pass
+
+    @abc.abstractmethod
+    def determiner_question_suivante(self) -> List[str]:
+        pass
+
+    @abc.abstractmethod
+    def proposer_metier(self, interets: str, competences: str, type_travail: str, description_personnelle: str) -> str:
         pass
